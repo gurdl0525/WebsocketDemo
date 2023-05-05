06db3c7ac9f4191ad3e0df6a69f27abb2fff497e
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-06 02:49:26 +0900

    ✨ :: binding exceptions

---
+++ src/main/kotlin/com/example/websocketdemo/global/error/ExceptionHandler.kt
@@ -0,0 +1,33 @@
+package com.example.websocketdemo.global.error
+
+import com.example.websocketdemo.global.exception.BusinessException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.validation.BindException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class ExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException::class)
+    protected fun handleBindException(e: BindException): BindErrorResponse = ErrorResponse.of(e)
+
+
+    @ExceptionHandler(BusinessException::class)
+    protected fun customExceptionHandle(e: BusinessException): ResponseEntity<ErrorResponse> {
+        val body = ErrorResponse.of(e.errorCode)
+        val status = HttpStatus.valueOf(e.errorCode.status())
+        return ResponseEntity(body, status)
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    protected fun handleHttpMessageNotReadable(e: HttpMessageNotReadableException) = ErrorResponse(
+        HttpStatus.BAD_REQUEST.value(),
+        e.httpInputMessage.toString(),
+        e.message ?: e.localizedMessage
+    )
+}
