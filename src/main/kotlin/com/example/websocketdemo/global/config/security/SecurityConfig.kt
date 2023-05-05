87e68bcd2631d7f2172263063eb71c28159f550d
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-06 01:16:35 +0900

    📝 :: SecurityConfig 생성

---
+++ src/main/kotlin/com/example/websocketdemo/global/config/security/SecurityConfig.kt
@@ -0,0 +1,44 @@
+package com.example.websocketdemo.global.config.security
+
+import com.example.websocketdemo.global.config.filter.FilterConfig
+import com.example.websocketdemo.global.config.jwt.JwtTokenParser
+import com.example.websocketdemo.global.config.jwt.JwtTokenResolver
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+    private val objectMapper: ObjectMapper,
+    private val jwtTokenParser: JwtTokenParser,
+    private val jwtTokenResolver: JwtTokenResolver
+) {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain =
+        http
+            .csrf().disable()
+            .formLogin().disable()
+            .cors().and()
+
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+
+            .authorizeHttpRequests()
+            .antMatchers("*").permitAll()
+            .anyRequest().permitAll()
+
+            .and().apply(FilterConfig(objectMapper, jwtTokenParser, jwtTokenResolver))
+
+            .and().build()
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+}
