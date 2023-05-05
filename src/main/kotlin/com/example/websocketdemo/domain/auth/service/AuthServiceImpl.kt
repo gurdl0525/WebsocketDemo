c46fdbffd003763ea73c2d53e9f7536316ca3a74
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-06 02:02:30 +0900

    ✨ :: login, reissue

--- src/main/kotlin/com/example/websocketdemo/domain/auth/service/AuthServiceImpl.kt
+++ src/main/kotlin/com/example/websocketdemo/domain/auth/service/AuthServiceImpl.kt
@@ -1,14 +1,23 @@
 package com.example.websocketdemo.domain.auth.service
 
-import com.example.websocketdemo.domain.auth.controller.dto.SignUpRequest
+import com.example.websocketdemo.domain.auth.controller.dto.request.LoginRequest
+import com.example.websocketdemo.domain.auth.controller.dto.request.SignUpRequest
+import com.example.websocketdemo.domain.auth.controller.dto.response.TokenResponse
 import com.example.websocketdemo.domain.auth.exception.DuplicatedAccountIdException
 import com.example.websocketdemo.domain.user.entity.User
+import com.example.websocketdemo.domain.user.exception.UserNotFoundException
 import com.example.websocketdemo.domain.user.repository.UserRepository
+import com.example.websocketdemo.global.common.UserFacade
+import com.example.websocketdemo.global.config.jwt.GenerateJwtAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.stereotype.Service
 
+@Service
 class AuthServiceImpl(
     private val userRepository: UserRepository,
-    private val passwordEncoder: BCryptPasswordEncoder
+    private val passwordEncoder: BCryptPasswordEncoder,
+    private val jwtProvider: GenerateJwtAdapter,
+    private val userFacade: UserFacade
 ): AuthService {
 
     override fun signUp(req: SignUpRequest) {
@@ -24,4 +33,13 @@ class AuthServiceImpl(
             )
         )
     }
+
+    override fun login(req: LoginRequest) =
+        if(userRepository.existsByAccountId(req.accountId)) {
+            jwtProvider.receiveToken(req.accountId)
+        }else{
+            throw UserNotFoundException
+        }
+
+    override fun reissue() = jwtProvider.receiveToken(userFacade.getUserByToken().accountId)
 }
