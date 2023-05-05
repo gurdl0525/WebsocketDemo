911acaccd0751a359f3c5e9ee473bb743c6901f8
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-06 02:46:24 +0900

    🐛 :: NFE -> dto initializing 오류

--- src/main/kotlin/com/example/websocketdemo/domain/auth/controller/AuthController.kt
+++ src/main/kotlin/com/example/websocketdemo/domain/auth/controller/AuthController.kt
@@ -2,26 +2,35 @@ package com.example.websocketdemo.domain.auth.controller
 
 import com.example.websocketdemo.domain.auth.controller.dto.request.LoginRequest
 import com.example.websocketdemo.domain.auth.controller.dto.request.SignUpRequest
-import com.example.websocketdemo.domain.auth.controller.dto.response.TokenResponse
 import com.example.websocketdemo.domain.auth.service.AuthService
 import org.springframework.http.HttpStatus
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/auth")
+@Validated
 class AuthController(
     private val authService: AuthService
 ) {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun signUp(req: SignUpRequest) = authService.signUp(req)
+    fun signUp(
+        @Valid @RequestBody
+        req: SignUpRequest
+    ) = authService.signUp(req)
 
     @PostMapping("/login")
-    fun login(req: LoginRequest) = authService.login(req)
+    fun login(
+        @Valid @RequestBody
+        req: LoginRequest
+    ) = authService.login(req)
 
     @PostMapping("/re-issue")
     fun reissue() = authService.reissue()
