06f0c46e0d9d76b174eb3de31eb58aa105ef4047
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-06 02:02:12 +0900

    ✨ :: login, reissue

--- src/main/kotlin/com/example/websocketdemo/domain/auth/service/AuthService.kt
+++ src/main/kotlin/com/example/websocketdemo/domain/auth/service/AuthService.kt
@@ -1,8 +1,14 @@
 package com.example.websocketdemo.domain.auth.service
 
-import com.example.websocketdemo.domain.auth.controller.dto.SignUpRequest
+import com.example.websocketdemo.domain.auth.controller.dto.request.LoginRequest
+import com.example.websocketdemo.domain.auth.controller.dto.request.SignUpRequest
+import com.example.websocketdemo.domain.auth.controller.dto.response.TokenResponse
 
 interface AuthService {
 
     fun signUp(req: SignUpRequest)
+
+    fun login(req: LoginRequest): TokenResponse
+
+    fun reissue(): TokenResponse
 }
