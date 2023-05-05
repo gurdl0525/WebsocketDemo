72fdab0364c40a3ed5bcfb5ca3cd17975280b9e4
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-06 01:14:08 +0900

    📝 :: JwtTokenParser 생성

---
+++ src/main/kotlin/com/example/websocketdemo/global/config/jwt/JwtTokenParser.kt
@@ -0,0 +1,44 @@
+package com.example.websocketdemo.global.config.jwt
+
+import com.example.websocketdemo.global.config.jwt.property.JwtProperties
+import com.example.websocketdemo.global.config.security.principal.AuthDetails
+import com.example.websocketdemo.global.config.security.principal.AuthDetailsService
+import com.example.websocketdemo.global.exception.ExpiredTokenException
+import com.example.websocketdemo.global.exception.InvalidTokenException
+import com.example.websocketdemo.global.exception.UnAuthorizedException
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.InvalidClaimException
+import io.jsonwebtoken.Jwts
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.stereotype.Component
+
+@Component
+class JwtTokenParser(
+    private val jwtProperties: JwtProperties,
+    private val authDetailsService: AuthDetailsService
+) {
+
+    private fun getSubject(token: String): String {
+        return try {
+            Jwts.parser()
+                .setSigningKey(jwtProperties.secretKey)
+                .parseClaimsJws(token).body.subject
+        } catch (e: Exception) {
+            when (e) {
+                is InvalidClaimException -> throw InvalidTokenException
+                is ExpiredJwtException -> throw ExpiredTokenException
+                else -> throw UnAuthorizedException
+            }
+        }
+    }
+
+    fun getAuthentication(token: String): Authentication {
+
+        val subject = getSubject(token)
+
+        val authDetails = authDetailsService.loadUserByUsername(subject) as AuthDetails
+
+        return UsernamePasswordAuthenticationToken(authDetails, "", authDetails.authorities)
+    }
+}
