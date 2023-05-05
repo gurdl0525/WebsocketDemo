4a625c85d69ca8717653effd2666a59cde953201
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-06 02:06:31 +0900

    📝 :: LoginRequest 생성

---
+++ src/main/kotlin/com/example/websocketdemo/domain/auth/controller/dto/request/LoginRequest.kt
@@ -0,0 +1,12 @@
+package com.example.websocketdemo.domain.auth.controller.dto.request
+
+import javax.validation.constraints.NotBlank
+
+data class LoginRequest (
+
+    @field:NotBlank(message = "공백 불가")
+    val accountId: String,
+
+    @field:NotBlank(message = "공백 불가")
+    val password: String
+)
