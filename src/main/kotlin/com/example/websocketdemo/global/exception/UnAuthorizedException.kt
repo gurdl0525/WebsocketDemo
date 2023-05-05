4851012b0356978a89b15e750c9284a9be8bf01b
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-06 01:09:20 +0900

    📝 :: UnAuthorizedException

---
+++ src/main/kotlin/com/example/websocketdemo/global/exception/UnAuthorizedException.kt
@@ -0,0 +1,5 @@
+package com.example.websocketdemo.global.exception
+
+import com.example.websocketdemo.global.error.ErrorCode
+
+object UnAuthorizedException: BusinessException(ErrorCode.UN_AUTHORIZED)
