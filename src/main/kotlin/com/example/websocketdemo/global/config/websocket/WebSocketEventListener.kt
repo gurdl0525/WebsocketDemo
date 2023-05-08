3e33a2b13890baeef366ef61418b5c7651b70adb
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-08 23:28:16 +0900

    📝 :: WebSocketEventListener 생성

---
+++ src/main/kotlin/com/example/websocketdemo/global/config/websocket/WebSocketEventListener.kt
@@ -0,0 +1,27 @@
+package com.example.websocketdemo.global.config.websocket
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.context.event.EventListener
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor
+import org.springframework.stereotype.Component
+import org.springframework.web.socket.messaging.SessionConnectedEvent
+import org.springframework.web.socket.messaging.SessionDisconnectEvent
+
+@Component
+class WebSocketEventListener{
+    private val logger: Logger = LoggerFactory.getLogger(WebSocketEventListener::class.java)
+
+    @EventListener
+    fun handleWebSocketConnectListener(event: SessionConnectedEvent?) {
+        logger.info("Received a new web socket connection")
+    }
+
+    @EventListener
+    fun handleWebSocketDisconnectListener(event: SessionDisconnectEvent) {
+        val headerAccessor = StompHeaderAccessor.wrap(event.message)
+        val username = headerAccessor.sessionAttributes!!["username"] as String? ?: "null"
+
+        logger.info("User Disconnected : $username")
+    }
+}
