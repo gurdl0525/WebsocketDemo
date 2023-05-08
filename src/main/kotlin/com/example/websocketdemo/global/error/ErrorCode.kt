51f711065aa96544e1018f0070b1733eab7b87ce
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-08 23:28:43 +0900

    📝 :: Error Message변경

--- src/main/kotlin/com/example/websocketdemo/global/error/ErrorCode.kt
+++ src/main/kotlin/com/example/websocketdemo/global/error/ErrorCode.kt
@@ -7,18 +7,18 @@ enum class ErrorCode(
 ){
 
     // Bad Request
-    BAD_REQUEST(400, "B01", "Bad Request"),
+    BAD_REQUEST(400, "B01", "잘못된 요청"),
 
     // Un Authorized
-    INVALID_TOKEN(401, "U01", "Invalid Token"),
-    EXPIRED_TOKEN(401, "U02", "Expired Token"),
-    UN_AUTHORIZED(401, "U03", "Un Authenticated"),
+    INVALID_TOKEN(401, "U01", "유효하지 않은 토큰"),
+    EXPIRED_TOKEN(401, "U02", "토큰의 사용기간이 만료되었습니다"),
+    UN_AUTHORIZED(401, "U03", "유효한 인증 정보가 부족하여 요청이 거부되었습니다"),
 
     // Not Found
-    USER_NOT_FOUND(404, "N01", "User Not Found"),
+    USER_NOT_FOUND(404, "N01", "유저를 찾을 수 없습니다"),
 
     // Conflict
-    DUPLICATED_ACCOUNT_ID(409, "C01", "Duplicated AccountId"),
+    DUPLICATED_ACCOUNT_ID(409, "C01", "중복된 아이디입니다"),
 
     // Internal Sever Error
     INTERNAL_SERVER_GLOBAL_ERROR(500, "I01", "Internal Server Error")
