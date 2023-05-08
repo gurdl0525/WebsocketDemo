a96594989577c50155472e25f83c39a045efad38
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-08 23:34:20 +0900

    📝 :: UserNotFound -> UnAuthorized

--- src/main/kotlin/com/example/websocketdemo/global/common/UserFacade.kt
+++ src/main/kotlin/com/example/websocketdemo/global/common/UserFacade.kt
@@ -1,9 +1,9 @@
 package com.example.websocketdemo.global.common
 
 import com.example.websocketdemo.domain.user.entity.User
-import com.example.websocketdemo.domain.user.exception.UserNotFoundException
 import com.example.websocketdemo.domain.user.repository.UserRepository
 import com.example.websocketdemo.global.config.security.principal.AuthDetails
+import com.example.websocketdemo.global.exception.UnAuthorizedException
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 
@@ -15,7 +15,7 @@ class UserFacade(
     fun getUserByToken(): User {
 
         val authDetails = (SecurityContextHolder.getContext().authentication
-            ?: throw UserNotFoundException).principal as AuthDetails
+            ?: throw UnAuthorizedException).principal as AuthDetails
 
         return userRepository.findByAccountId(authDetails.username)!!
     }
