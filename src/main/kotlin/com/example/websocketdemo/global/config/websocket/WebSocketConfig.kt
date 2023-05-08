bd7fa1f5412f8d6abb55b4692b1232a73bde0880
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-08 23:30:35 +0900

    ♻️ :: code refactoring

--- src/main/kotlin/com/example/websocketdemo/global/config/websocket/WebSocketConfig.kt
+++ src/main/kotlin/com/example/websocketdemo/global/config/websocket/WebSocketConfig.kt
@@ -1,7 +1,6 @@
 package com.example.websocketdemo.global.config.websocket
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
@@ -11,13 +10,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 class WebSocketConfig: WebSocketMessageBrokerConfigurer {
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
-        registry.addEndpoint("/ws")
-            .withSockJS()
-    }
-
-    override fun configureMessageBroker(config: MessageBrokerRegistry) {
-        config.setApplicationDestinationPrefixes("/app")
-        config.enableSimpleBroker("/topic")
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS()
     }
 
 }
