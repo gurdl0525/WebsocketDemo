36701476e9a5a7023ac86ce92f7311b3079c619f
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-10 16:20:55 +0900

    📝 :: ClientProperties

---
+++ src/main/kotlin/com/example/websocketdemo/global/config/websocket/property/ClientProperties.kt
@@ -0,0 +1,8 @@
+package com.example.websocketdemo.global.config.websocket.property
+
+object ClientProperties {
+
+    const val USER_KEY: String = "user_key"
+
+    const val ROOM_KEY: String = "room_key"
+}
