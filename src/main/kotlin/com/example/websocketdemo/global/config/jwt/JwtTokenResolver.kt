3059e5a50a669dfd6b8ab3ac3fd70e14f36bfb82
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-10 16:24:50 +0900

    ✨ :: resolveToken 오버로딩

--- src/main/kotlin/com/example/websocketdemo/global/config/jwt/JwtTokenResolver.kt
+++ src/main/kotlin/com/example/websocketdemo/global/config/jwt/JwtTokenResolver.kt
@@ -1,14 +1,20 @@
 package com.example.websocketdemo.global.config.jwt
 
+import com.corundumstudio.socketio.SocketIOClient
 import org.springframework.stereotype.Component
 import java.util.regex.Pattern
 import javax.servlet.http.HttpServletRequest
+
 
 @Component
 class JwtTokenResolver {
 
     fun resolveToken(httpServletRequest: HttpServletRequest) = parseToken(
         httpServletRequest.getHeader("Authorization")
+    )
+
+    fun resolveToken(socketIOClient: SocketIOClient) = parseToken(
+        socketIOClient.handshakeData.httpHeaders["Authorization"]
     )
 
     private fun parseToken(token: String?) =
