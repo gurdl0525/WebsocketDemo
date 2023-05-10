c8d9e15a211e7786876d6aaa603dbadc4634a270
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-10 16:22:34 +0900

    📝 :: RoomUserNotFoundException

---
+++ src/main/kotlin/com/example/websocketdemo/global/exception/RoomUserNotFoundException.kt
@@ -0,0 +1,5 @@
+package com.example.websocketdemo.global.exception
+
+import com.example.websocketdemo.global.error.ErrorCode
+
+object RoomUserNotFoundException: BusinessException(ErrorCode.ROOM_USER_NOT_FOUND)
