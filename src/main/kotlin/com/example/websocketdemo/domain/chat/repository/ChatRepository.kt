837ea3e1ce51cff4d7fec234571e0d8ee8b18615
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-10 16:23:52 +0900

    ✨ :: findTopByRoomAndAndCreatedAtBeforeOrderByIdAsc

--- src/main/kotlin/com/example/websocketdemo/domain/chat/repository/ChatRepository.kt
+++ src/main/kotlin/com/example/websocketdemo/domain/chat/repository/ChatRepository.kt
@@ -1,10 +1,15 @@
 package com.example.websocketdemo.domain.chat.repository
 
 import com.example.websocketdemo.domain.chat.entity.Chat
+import com.example.websocketdemo.domain.chat.entity.Room
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
 import java.util.UUID
 
 @Repository
 interface ChatRepository: JpaRepository<Chat, UUID> {
+
+    fun findTopByRoomAndAndCreatedAtBeforeOrderByIdAsc(room: Room, localDateTime: LocalDateTime): List<Chat>?
+
 }
