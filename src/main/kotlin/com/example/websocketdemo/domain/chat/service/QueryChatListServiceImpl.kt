38b505c6296e38c341bd7c9a91189566797d8e8d
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-10 16:16:24 +0900

    📝 :: QueryChatListService 구현

---
+++ src/main/kotlin/com/example/websocketdemo/domain/chat/service/QueryChatListServiceImpl.kt
@@ -0,0 +1,39 @@
+package com.example.websocketdemo.domain.chat.service
+
+import com.example.websocketdemo.domain.chat.controller.dto.response.ChatResponse
+import com.example.websocketdemo.domain.chat.controller.dto.response.QueryChatListResponse
+import com.example.websocketdemo.domain.chat.entity.RoomUser
+import com.example.websocketdemo.domain.chat.facade.RoomUserFacade
+import com.example.websocketdemo.domain.chat.repository.ChatRepository
+import com.example.websocketdemo.domain.user.entity.User
+import com.example.websocketdemo.domain.user.facade.UserFacade
+import com.example.websocketdemo.global.exception.InternalServerErrorException
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
+import java.util.*
+
+@Service
+class QueryChatListServiceImpl(
+    private val userFacade: UserFacade,
+    private val roomUserFacade: RoomUserFacade,
+    private val chatRepository: ChatRepository
+): QueryChatListService {
+
+    @Transactional
+    override fun execute(roomId: UUID, localDateTime: LocalDateTime): QueryChatListResponse {
+        val user: User = userFacade.getCurrentUser()
+        val roomUser: RoomUser = roomUserFacade.getById(roomId, user.id!!)
+
+        val chatList = chatRepository.findTopByRoomAndAndCreatedAtBeforeOrderByIdAsc(roomUser.room, localDateTime)
+            ?: throw InternalServerErrorException
+
+        roomUser.updateLastReadTime()
+
+        return QueryChatListResponse(
+            chatList
+                .map { ChatResponse.of(it, it.sender === user) }
+                .toList()
+        )
+    }
+}
