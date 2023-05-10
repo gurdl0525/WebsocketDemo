9728a7e341a43a3a982214f3d9d0fc1e3ab30386
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-10 16:20:15 +0900

    📝 :: SocketRoomFacade

---
+++ src/main/kotlin/com/example/websocketdemo/global/config/websocket/facade/SocketRoomFacade.kt
@@ -0,0 +1,42 @@
+package com.example.websocketdemo.global.config.websocket.facade
+
+import com.corundumstudio.socketio.SocketIOClient
+import com.example.websocketdemo.domain.chat.entity.Room
+import com.example.websocketdemo.domain.chat.facade.RoomFacade
+import com.example.websocketdemo.domain.chat.facade.RoomUserFacade
+import com.example.websocketdemo.domain.user.entity.User
+import com.example.websocketdemo.global.config.websocket.property.ClientProperties
+import org.springframework.stereotype.Component
+import java.lang.String
+import java.util.UUID
+import java.util.function.Consumer
+
+
+@Component
+class SocketRoomFacade(
+    private val roomFacade: RoomFacade,
+    private val roomUserFacade: RoomUserFacade,
+) {
+
+    fun joinAllRoom(socketIOClient: SocketIOClient, user: User) {
+
+        val roomUserList = roomUserFacade.getListByUser(user)
+
+        roomUserList!!
+            .map { socketIOClient.joinRoom(String.valueOf(it.room.id)) }
+            .toList()
+    }
+
+    fun joinOneRoom(socketIOClient: SocketIOClient, user: User, roomId: UUID) {
+
+        val room: Room = roomFacade.getRoomById(roomId)
+
+        roomUserFacade.checkRoomUserExist(room, user)
+
+        socketIOClient.let {
+            it.allRooms.forEach(Consumer { room: kotlin.String? -> socketIOClient.leaveRoom(room) })
+            it[ClientProperties.ROOM_KEY] = roomId
+            it.joinRoom(String.valueOf(roomId))
+        }
+    }
+}
