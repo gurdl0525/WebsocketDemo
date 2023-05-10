a4b5a9fa08d30dd0953b5445e9f25729b6a1a6eb
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-10 16:21:53 +0900

    📝 :: SocketUtil

---
+++ src/main/kotlin/com/example/websocketdemo/global/config/websocket/util/SocketUtil.kt
@@ -0,0 +1,25 @@
+package com.example.websocketdemo.global.config.websocket.util
+
+import com.corundumstudio.socketio.SocketIOClient
+import com.example.websocketdemo.global.config.websocket.property.ClientProperties
+import com.example.websocketdemo.global.exception.InvalidUserException
+import com.example.websocketdemo.global.exception.RoomNotFoundException
+import java.util.UUID
+
+
+object SocketUtil {
+
+    fun getUserId(socketIOClient: SocketIOClient): UUID {
+        if (!socketIOClient.has(ClientProperties.USER_KEY)) {
+            throw InvalidUserException
+        }
+        return socketIOClient.get(ClientProperties.USER_KEY)
+    }
+
+    fun getRoomId(socketIOClient: SocketIOClient): UUID {
+        if (!socketIOClient.has(ClientProperties.ROOM_KEY)) {
+            throw RoomNotFoundException
+        }
+        return socketIOClient.get(ClientProperties.ROOM_KEY)
+    }
+}
