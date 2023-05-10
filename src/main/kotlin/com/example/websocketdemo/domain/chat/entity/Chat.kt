8a0375b380ac528b9fe5e768f7ae3c8254175a32
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-10 16:11:57 +0900

    📝 :: room, createdAt, updatedAt 추가

--- src/main/kotlin/com/example/websocketdemo/domain/chat/entity/Chat.kt
+++ src/main/kotlin/com/example/websocketdemo/domain/chat/entity/Chat.kt
@@ -1,30 +1,42 @@
 package com.example.websocketdemo.domain.chat.entity
 
 import com.example.websocketdemo.domain.user.entity.User
+import java.time.LocalDateTime
 import java.util.*
 import javax.persistence.*
 
 @Entity
 class Chat(
     id: UUID? = null,
-    content: String,
-    sender: String,
-    target: User,
+    message: String,
+    sender: User,
+    room: Room,
+    updatedAt: LocalDateTime? = null
 ) {
 
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
     var id: UUID? = id
         protected set
 
-    @Column(name = "content", length = 1000, nullable = false)
-    var content:String = content
-        protected set
-
-    @Column(name = "sender", length = 15, nullable = false)
-    var sender: String = sender
+    @Column(name = "message", length = 1000, nullable = false)
+    var message:String = message
         protected set
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "target", nullable = false)
-    var target: User = target
+    @JoinColumn(name = "sender", nullable = false)
+    var sender: User = sender
+        protected set
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room", nullable = false)
+    var room: Room = room
+        protected set
+
+    @Column(name = "created_at", nullable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now()
+        protected set
+
+    @Column(name = "updated_at", nullable = true)
+    var updatedAt: LocalDateTime? = updatedAt
 }
