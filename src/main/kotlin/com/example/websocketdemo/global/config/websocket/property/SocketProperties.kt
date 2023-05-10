88f159133c21afaae035e2417df13b6efdef7079
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-10 16:21:09 +0900

    📝 :: SocketProperties

---
+++ src/main/kotlin/com/example/websocketdemo/global/config/websocket/property/SocketProperties.kt
@@ -0,0 +1,10 @@
+package com.example.websocketdemo.global.config.websocket.property
+
+object SocketProperties {
+
+    const val CHAT = "chat"
+
+    const val JOIN = "join"
+
+    const val ERROR = "error"
+}
