e10a1d8da046aaca73b7aac0008d18c8114faf9c
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-15 10:18:03 +0900

    📝 :: InternalServerErrorException -> RoomNotFoundException

--- src/main/kotlin/com/example/websocketdemo/domain/chat/service/QueryChatListServiceImpl.kt
+++ src/main/kotlin/com/example/websocketdemo/domain/chat/service/QueryChatListServiceImpl.kt
@@ -3,6 +3,7 @@ package com.example.websocketdemo.domain.chat.service
 import com.example.websocketdemo.domain.chat.controller.dto.response.ChatResponse
 import com.example.websocketdemo.domain.chat.controller.dto.response.QueryChatListResponse
 import com.example.websocketdemo.domain.chat.entity.RoomUser
+import com.example.websocketdemo.domain.chat.exception.RoomNotFoundException
 import com.example.websocketdemo.domain.chat.facade.RoomUserFacade
 import com.example.websocketdemo.domain.chat.repository.ChatRepository
 import com.example.websocketdemo.domain.user.entity.User
@@ -22,11 +23,13 @@ class QueryChatListServiceImpl(
 
     @Transactional
     override fun execute(roomId: UUID, localDateTime: LocalDateTime): QueryChatListResponse {
+
         val user: User = userFacade.getCurrentUser()
+
         val roomUser: RoomUser = roomUserFacade.getById(roomId, user.id!!)
 
         val chatList = chatRepository.findTopByRoomAndAndCreatedAtBeforeOrderByIdAsc(roomUser.room, localDateTime)
-            ?: throw InternalServerErrorException
+            ?: throw RoomNotFoundException
 
         roomUser.updateLastReadTime()
 
