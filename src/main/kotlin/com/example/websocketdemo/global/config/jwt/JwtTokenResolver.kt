dcc32734264ffb2ed8e81fe0d178c4791a43137a
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-15 10:20:06 +0900

    ⚡️ :: handshaking 할 때 토큰 참조 가능하도록 변경

--- src/main/kotlin/com/example/websocketdemo/global/config/jwt/JwtTokenResolver.kt
+++ src/main/kotlin/com/example/websocketdemo/global/config/jwt/JwtTokenResolver.kt
@@ -1,6 +1,7 @@
 package com.example.websocketdemo.global.config.jwt
 
 import com.corundumstudio.socketio.SocketIOClient
+import com.example.websocketdemo.global.exception.UnAuthorizedException
 import org.springframework.stereotype.Component
 import java.util.regex.Pattern
 import javax.servlet.http.HttpServletRequest
@@ -9,13 +10,22 @@ import javax.servlet.http.HttpServletRequest
 @Component
 class JwtTokenResolver {
 
-    fun resolveToken(httpServletRequest: HttpServletRequest) = parseToken(
+    fun resolveToken(httpServletRequest: HttpServletRequest): String? = parseToken(
         httpServletRequest.getHeader("Authorization")
     )
 
-    fun resolveToken(socketIOClient: SocketIOClient) = parseToken(
-        socketIOClient.handshakeData.httpHeaders["Authorization"]
-    )
+    fun resolveToken(socketIOClient: SocketIOClient): String {
+
+        return parseToken(socketIOClient.handshakeData.urlParams["auth"])
+            ?: throw UnAuthorizedException
+    }
+
+    private fun parseToken(token: List<String>?): String? =
+        if (!token.isNullOrEmpty() && Pattern.matches("[(a-zA-Z0-9-._~+/=*)]{30,600}", token[0])) {
+            token[0]
+        } else {
+            null
+        }
 
     private fun parseToken(token: String?) =
         if (token != null && Pattern.matches("Bearer [(a-zA-Z0-9-._~+/=*)]{30,600}", token)) {
