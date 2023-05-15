288ec7e7910466f9a5d1fd831edbb619ef88a36d
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-15 10:18:53 +0900

    🐛 :: java.lang.ClassCastException: org.springframework.security.core.userdetails.User cannot be cast to User

--- src/main/kotlin/com/example/websocketdemo/domain/user/facade/UserFacade.kt
+++ src/main/kotlin/com/example/websocketdemo/domain/user/facade/UserFacade.kt
@@ -1,43 +1,34 @@
 package com.example.websocketdemo.domain.user.facade
 
 import com.corundumstudio.socketio.SocketIOClient
-import com.example.websocketdemo.domain.auth.exception.PasswordMismatchedException
 import com.example.websocketdemo.domain.user.entity.User
 import com.example.websocketdemo.domain.user.exception.UserNotFoundException
 import com.example.websocketdemo.domain.user.repository.UserRepository
-import com.example.websocketdemo.global.config.security.principal.AuthDetails
 import com.example.websocketdemo.global.config.websocket.util.SocketUtil
 import com.example.websocketdemo.global.exception.UnAuthorizedException
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Component
-import java.util.UUID
 
 @Component
 class UserFacade(
-    private val userRepository: UserRepository,
-    private val passwordEncoder: BCryptPasswordEncoder
+    private val userRepository: UserRepository
 ){
 
     fun getCurrentUser(): User {
 
-        val authDetails = (SecurityContextHolder.getContext().authentication
-            ?: throw UnAuthorizedException).principal as AuthDetails
+        val authentication: Authentication = SecurityContextHolder.getContext().authentication
 
-        return userRepository.findByAccountId(authDetails.username)!!
+        val userName: String = authentication.name
+
+        return findByAccountId(userName) ?: throw UnAuthorizedException
     }
 
     fun getCurrentUser(socketIOClient: SocketIOClient) = userRepository.findByIdOrNull(SocketUtil.getUserId(socketIOClient))
         ?: throw UnAuthorizedException
 
-    fun getUserByAccountId(accountId: String) = userRepository.findByAccountId(accountId) ?: throw UserNotFoundException
+    fun getUserByAccountId(accountId: String) = findByAccountId(accountId) ?: throw UserNotFoundException
 
-    fun getUserById(id: UUID) = userRepository.findByIdOrNull(id) ?: throw UserNotFoundException
-
-    fun checkPassword(user: User, password: String?) {
-        if (!passwordEncoder.matches(password, user.password)) {
-            throw PasswordMismatchedException
-        }
-    }
+    private fun findByAccountId(accountId: String) = userRepository.findByAccountId(accountId)
 }
