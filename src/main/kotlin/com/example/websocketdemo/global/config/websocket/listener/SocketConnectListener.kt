bf8e4e5d206f6b4e99ecb85bb621fcdd6aeb1386
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-15 10:25:34 +0900

    ♻️ :: 중복코드 단편화

--- src/main/kotlin/com/example/websocketdemo/global/config/websocket/listener/SocketConnectListener.kt
+++ src/main/kotlin/com/example/websocketdemo/global/config/websocket/listener/SocketConnectListener.kt
@@ -4,8 +4,6 @@ import com.corundumstudio.socketio.SocketIOClient
 import com.corundumstudio.socketio.annotation.OnConnect
 import com.example.websocketdemo.domain.user.entity.User
 import com.example.websocketdemo.domain.user.facade.UserFacade
-import com.example.websocketdemo.global.config.jwt.JwtTokenParser
-import com.example.websocketdemo.global.config.jwt.JwtTokenResolver
 import com.example.websocketdemo.global.config.websocket.facade.SocketRoomFacade
 import com.example.websocketdemo.global.config.websocket.property.ClientProperties
 import org.springframework.stereotype.Component
@@ -13,8 +11,6 @@ import org.springframework.stereotype.Component
 
 @Component
 class SocketConnectListener(
-    private val jwtTokenResolver: JwtTokenResolver,
-    private val jwtTokenParser: JwtTokenParser,
     private val socketRoomFacade: SocketRoomFacade,
     private val userFacade: UserFacade
 ){
@@ -22,9 +18,7 @@ class SocketConnectListener(
     @OnConnect
     fun onConnect(socketIOClient: SocketIOClient) {
 
-        val accountId: String = jwtTokenParser.getAuthentication(jwtTokenResolver.resolveToken(socketIOClient)!!).name
-
-        val user: User = userFacade.getUserByAccountId(accountId)
+        val user: User = userFacade.getCurrentUser()
 
         socketIOClient.set(ClientProperties.USER_KEY, user.id)
 
