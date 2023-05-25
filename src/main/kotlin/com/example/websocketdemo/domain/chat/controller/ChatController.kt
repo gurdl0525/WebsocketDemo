9d751461ba9af947fa7f53f4ee6d0282f3c05fba
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-25 23:39:02 +0900

    ✨ :: CreateRoom

--- src/main/kotlin/com/example/websocketdemo/domain/chat/controller/ChatController.kt
+++ src/main/kotlin/com/example/websocketdemo/domain/chat/controller/ChatController.kt
@@ -1,19 +1,33 @@
 package com.example.websocketdemo.domain.chat.controller
 
+import com.example.websocketdemo.domain.chat.controller.dto.request.CreateRoomRequest
+import com.example.websocketdemo.domain.chat.controller.dto.response.CreateRoomResponse
 import com.example.websocketdemo.domain.chat.controller.dto.response.QueryChatListResponse
 import com.example.websocketdemo.domain.chat.controller.dto.response.QueryRoomListResponse
+import com.example.websocketdemo.domain.chat.service.CreateRoomService
 import com.example.websocketdemo.domain.chat.service.QueryChatListService
 import com.example.websocketdemo.domain.chat.service.QueryMyRoomListService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import java.time.LocalDateTime
-import java.util.UUID
+import java.util.*
+import javax.validation.Valid
+
 
 @RequestMapping("/chat")
 @RestController
 class ChatController(
     private val queryChatListService: QueryChatListService,
     private val queryMyRoomListService: QueryMyRoomListService,
+    private val createRoomService: CreateRoomService
 ) {
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/room")
+    fun createPrivateChatRoom(
+        @RequestBody @Valid
+        request: CreateRoomRequest
+    ): CreateRoomResponse = createRoomService.exec(request)
 
     @GetMapping("/{room-id}")
     fun queryChatList(
