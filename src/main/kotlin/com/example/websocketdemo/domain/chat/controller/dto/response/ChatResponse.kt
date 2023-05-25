74f1961d964aa0db0df98eb667bb8135746a332b
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-05-25 23:40:38 +0900

    📝 :: @JsonProperty 삭제

--- src/main/kotlin/com/example/websocketdemo/domain/chat/controller/dto/response/ChatResponse.kt
+++ src/main/kotlin/com/example/websocketdemo/domain/chat/controller/dto/response/ChatResponse.kt
@@ -3,26 +3,19 @@ package com.example.websocketdemo.domain.chat.controller.dto.response
 import com.example.websocketdemo.domain.chat.entity.Chat
 import com.example.websocketdemo.domain.user.controller.dto.response.UserResponse
 import com.example.websocketdemo.global.util.DateUtil
-import com.fasterxml.jackson.annotation.JsonProperty
-import java.util.UUID
+import java.util.*
 
 data class ChatResponse(
 
-    @JsonProperty("room_id")
     val roomId: UUID,
 
-    @JsonProperty("is_mine")
     val isMine: Boolean,
 
-    @JsonProperty("sent_at")
     val sentAt: String,
 
-    @JsonProperty("user")
     val user: UserResponse,
 
-    @JsonProperty("message")
     val message: String,
-
 
 ){
     companion object {
